7726c1b144b3271075ce8c894fbf77b553e78a59
JasonPink79 <jaysunrose@aol.com>
2021-03-10 02:33:06 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["Voldemort", "Sauron", "Supreme Leader Snoke", "Thanos"];
+let action = [
+  "cursed",
+  "annhilated with his dark army of trolls and orcs",
+  "ordered General Grievous to destroy",
+  "removed from existence with his infinity gauntlet"
+];
+let what = [
+  "my coding project",
+  "my love life",
+  "all my excuses",
+  "all my code"
+];
+let when = [
+  "before the class",
+  "when I least wanted it to happen",
+  "right when I finished",
+  "just in time",
+  "as I secretly requested",
+  "at the most inconvenient time"
+];
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let target = document.querySelector("#excuse");
+
+  let sentence =
+    who[getRndInteger(0, who.length - 1)] +
+    " " +
+    action[getRndInteger(0, action.length - 1)] +
+    " " +
+    what[getRndInteger(0, what.length - 1)] +
+    " " +
+    when[getRndInteger(0, when.length - 1)];
+
+  target.innerHTML = sentence;
 };
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
